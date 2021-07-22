2850a3867c0fd3370ec19e3146a2fe585c06a75c
David Ku <hola@davidku.dev>
2021-07-22 18:59:32 -0500

    se corrigen galerias y botones

--- js/galeria.js
+++ js/galeria.js
@@ -1,12 +1,8 @@
-$('#basicExample2').justifiedGallery({
-  lastRow : 'nojustify',
-  margins : 0
+var elem = document.querySelector(".grid-container");
+var msnry = new Masonry(elem, {
+  // options
+  itemSelector: ".grid-item",
+  columnWidth: 230,
+  gutter: 20,
+  isFitWidth: true
 });
-      var elem = document.querySelector(".grid-container");
-      var msnry = new Masonry(elem, {
-        // options
-        itemSelector: ".grid-item",
-        columnWidth: 230,
-        gutter: 20,
-        isFitWidth: true
-      });
